8d60aa9dec66a09fd8e66b902bb1b9d5660baffd
ace <vuong753333@gmail.com>
2024-08-26 03:30:29 +0700

    feat: add setup function

---
+++ lua/presetup/util/init.lua
@@ -0,0 +1,188 @@
+local Util = require("lazy.core.util")
+
+---@class util: UtilCore
+---@field config Config
+---@field lsp util.lsp
+---@field cmp util.cmp
+local M = {}
+
+setmetatable(M, {
+	__index = function(t, k)
+		if Util[k] then
+			return Util[k]
+		end
+		t[k] = require("util." .. k)
+		return t[k]
+	end,
+})
+function M.is_win()
+	return vim.uv.os_uname().sysname:find("Windows") ~= nil
+end
+---@param name string
+function M.get_plugin(name)
+	return require("lazy.core.config").spec.plugins[name]
+end
+
+---@param name string
+---@param path string?
+function M.get_plugin_path(name, path)
+	local plugin = M.get_plugin(name)
+	path = path and "/" .. path or ""
+	return plugin and (plugin.dir .. path)
+end
+
+---@param plugin string
+function M.has(plugin)
+	return M.get_plugin(plugin) ~= nil
+end
+
+---@param fn fun()
+function M.on_very_lazy(fn)
+	vim.api.nvim_create_autocmd("User", {
+		pattern = "VeryLazy",
+		callback = function()
+			fn()
+		end,
+	})
+end
+
+---@generic T
+---@param t T[]
+---@param key string
+---@param values T[]
+---@return T[]?
+function M.extend(t, key, values)
+	local keys = vim.split(key, ".", { plain = true })
+	for i = 1, #keys do
+		local k = keys[i]
+		t[k] = t[k] or {}
+		if type(t) ~= "table" then
+			return
+		end
+		t = t[k]
+	end
+	return vim.list_extend(t, values)
+end
+
+function M.is_loaded(name)
+	local Config = require("lazy.core.config")
+	return Config.plugins[name] and Config.plugins[name]._.loaded
+end
+---@param name string
+---
+---@param fn fun(name:string)
+function M.on_load(name, fn)
+	if M.is_loaded(name) then
+		fn(name)
+	else
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LazyLoad",
+			callback = function(event)
+				if event.data == name then
+					fn(name)
+					return true
+				end
+			end,
+		})
+	end
+end
+---@param name string
+function M.opts(name)
+	local plugin = M.get_plugin(name)
+	if not plugin then
+		return {}
+	end
+	local Plugin = require("lazy.core.plugin")
+	return Plugin.values(plugin, "opts", false)
+end
+M.config = {
+	icons = {
+		diagnostics = {
+			Error = " ",
+			Warn = " ",
+			Hint = " ",
+			Info = " ",
+		},
+		git = {
+			added = " ",
+			modified = " ",
+			removed = " ",
+		},
+		kinds = {
+			Array = " ",
+			Boolean = "󰨙 ",
+			Class = " ",
+			Codeium = "󰘦 ",
+			Color = " ",
+			Control = " ",
+			Collapsed = " ",
+			Constant = "󰏿 ",
+			Constructor = " ",
+			Copilot = " ",
+			Enum = " ",
+			EnumMember = " ",
+			Event = " ",
+			Field = " ",
+			File = " ",
+			Folder = " ",
+			Function = "󰊕 ",
+			Interface = " ",
+			Key = " ",
+			Keyword = " ",
+			Method = "󰊕 ",
+			Module = " ",
+			Namespace = "󰦮 ",
+			Null = " ",
+			Number = "󰎠 ",
+			Object = " ",
+			Operator = " ",
+			Package = " ",
+			Property = " ",
+			Reference = " ",
+			Snippet = "",
+			String = " ",
+			Struct = "󰆼 ",
+			TabNine = "󰏚 ",
+			Text = " ",
+			TypeParameter = " ",
+			Unit = " ",
+			Value = " ",
+			Variable = "󰀫 ",
+		},
+	},
+	---@type table<string, string[]|boolean>?
+	kind_filter = {
+		default = {
+			"Class",
+			"Constructor",
+			"Enum",
+			"Field",
+			"Function",
+			"Interface",
+			"Method",
+			"Module",
+			"Namespace",
+			"Package",
+			"Property",
+			"Struct",
+			"Trait",
+		},
+		markdown = false,
+		help = false,
+		lua = {
+			"Class",
+			"Constructor",
+			"Enum",
+			"Field",
+			"Function",
+			"Interface",
+			"Method",
+			"Module",
+			"Namespace",
+			"Property",
+			"Struct",
+			"Trait",
+		},
+	},
+}
+_G.Ace = M
